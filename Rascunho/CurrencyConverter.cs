04da5cb96e8be460e974ed86ac70c3cd815f1333
Cristiano Castiel <cristianoct03@gmail.com>
2024-01-03 17:10:08 -0300

    Exercise finished

---
+++ Rascunho/CurrencyConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class CurrencyConverter
+    {
+        static private double IOF = 6.00;
+
+        static public double ConverterReaisToDollar(double DollarExchangeRate, double quantity)
+        {
+            double total = DollarExchangeRate * quantity;
+            return +total + total / 100 * IOF;
+        }
+    }
+}
---
+++ Rascunho/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+
+            Console.Write("What is the dollar exchange rate:");
+            double exchangeRate = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("How many dollars will you buy:");
+            double dollarBuy = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           
+            double totalQuantityReais = CurrencyConverter.ConverterReaisToDollar(exchangeRate, dollarBuy);
+            Console.WriteLine("Amount to be paid in reais: " + totalQuantityReais.ToString("F2", CultureInfo.InvariantCulture) );
+            Console.ReadKey();
+        }      
+
+    }
+}
